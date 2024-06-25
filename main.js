59dd8405f4ef292cf7d81fca552ea2cebdadcc1e
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-06-25 18:06:55 +0700

    finished review

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-const button = document.getElementById("click-me").addEventListener("click", ()=> {
-    setTimeout(()=>{
-        alert("I was clicked 2s ago");
-    }, 2000);
+
+function checkEmail(string){
+    if(string.includes("@") && string.includes('.')) return true;
+    return false;
+}
+
+function checkPassword(string){
+    return string.length >= 5;
+}
+
+const validation = document.getElementById("form").addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const emailInput = document.getElementById("email").value;
+    const passwordInput = document.getElementById("password").value;
+    
+    if(!checkEmail(emailInput) && !checkPassword(passwordInput)){
+        return alert("Failed to login");
+    } 
+    
+    const tempStore = {
+        'email' : emailInput,
+        'password': passwordInput
+    };
+
+    localStorage.setItem(emailInput, JSON.stringify(tempStore));
+
 });
